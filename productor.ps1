46fcc5cb748227c05a87fa3f5e23ddc9c650863d
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-03-24 14:09:47 -0600

    PowerShell
    Se agrego el primer archivo de powershell

---
+++ productor.ps1
@@ -0,0 +1,18 @@
+﻿#Autor: Andres Hernandez Mata
+#Fecha: 24/03/2021
+#Version: 1.0
+
+#Nos movemos al directorio home del usuario
+Set-Location $HOME
+
+#Preguntar al usuario su nombre
+$nombre = Read-Host "¿Cual es su nombre? "
+
+#Preguntar al usuario su apellido
+$apellido = Read-Host "¿Cual es su primer apellido? "
+
+Write-Host "Hola $nombre $apellido" -ForegroundColor Yellow
+
+Write-Host "*** Directorio actual ***"
+Get-Location
+
