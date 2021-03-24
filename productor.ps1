8a869729bf26190a991fad34ad5e7b7f6952fb3c
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-03-24 14:35:15 -0600

    Generar archivos de texto
    Haciendo un ping y obtener el tamaño del directorio home

--- productor.ps1
+++ productor.ps1
@@ -13,6 +13,13 @@ $apellido = Read-Host "¿Cual es su primer apellido? "
 
 Write-Host "Hola $nombre $apellido" -ForegroundColor Yellow
 
-Write-Host "*** Directorio actual ***"
+Write-Host "*** Directorio actual ***" -ForegroundColor Red
 Get-Location
 
+Write-Host "*** Probando la conectividad con un ping a la computadora local 127.0.0.1 ***" -ForegroundColor Red
+Test-Connection 127.0.0.1 > ping.txt | Format-Table -AutoSize
+Write-Host "Se genero el archivo ping.txt con el resultado" -ForegroundColor Red
+
+Write-Host "*** Consultado el tamaño del directorio home del usuario ***"
+Get-ChildItem -Path $HOME -File -Recurse | Measure-Object -Property Length -Sum > size.txt
+Write-Host "Se genero el archivo size.txt con el resultado esperado"
