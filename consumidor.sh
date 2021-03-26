8c3c8b56438ad29ac6bc3508e60ce31a9f13aa68
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-03-26 12:29:14 -0600

    Mensajes de salida
    Se modificaron algunos cambios en las variables y mensajes

--- consumidor.sh
+++ consumidor.sh
@@ -9,16 +9,17 @@ echo "\e[91m*** Borrar archivo o directorio ***\e[0m"
 echo "\e[93mIngresar la ruta del directoio o archivo que quiere borrar\e[0m"
 read ruta
 
-echo "\e[91m*** Comprando si es un directorio o archivo ***\e[0m"
+echo "\e[91m*** Verificando si la ruta ingresada existe ***\e[0m"
 if [ -e "$ruta" ];
 then 
-	echo "\e[92mEl elemento si exite en $ruta\e[0m"
+	echo "\e[92mLa ruta si exite en $ruta\e[0m"
 else 
-   	echo "\e[91mEl elemento no exite en $ruta\e[0m"
+   	echo "\e[91mLa ruta no exite en $ruta\e[0m"
 	echo "\e[104mAdios $1 $2\e[0m"
 	exit 1
 fi
 
+echo "\e[91m*** Verificando si es un directorio o archivo ***\e[0m"
 if [ -f "$ruta" ]
 then
 	echo "\e[92mLa ubicacion $ruta indica que es un archivo\e[0m"
@@ -26,7 +27,7 @@ then
 elif [ -d "$ruta" ]
 then
 	echo "\e[92mLa ubicacion $ruta indica que es un directorio\e[0m"
-	directoio=true
+	directorio=true
 fi
 
 echo "\e[91m*** Comprobando si tiene permisos de lectura y/o escritura ***\e[0m"
