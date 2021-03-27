8f8663ed6d033826e514a15553ba4e6cb50e3a1e
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-03-27 05:19:04 -0600

    Se crean funciones para cada paso del algoritmo
    Funciones en bash para el laboratorio

--- productor.sh
+++ productor.sh
@@ -1,69 +1,140 @@
 #!/usr/bin/bash
 
+<<COMMENT
+    Autor: Andres Hernandez Mata
+    Fecha: 24/03/2021
+    Version: 2.0
+COMMENT
+
 #Funcion para simular un progress bar con movimiento
 function progress_bar(){
-    echo -ne '\e[42m#####                     (33%)\e[0m\r'
+    echo -ne '\e[92m#####                     (33%)\e[0m\r'
     sleep 1
-    echo -ne '\e[42m#############             (66%)\e[0m\r'
+    echo -ne '\e[92m#############             (66%)\e[0m\r'
     sleep 1
-    echo -ne '\e[42m#######################   (100%)\e[0m\r'
+    echo -ne '\e[92m#######################   (100%)\e[0m\r'
     echo -ne '\n'
 }
 
+clear
+
 cd $HOME
 
-echo -e "\e[93m¿Cual es su nombre?\e[0m"
-read nombre
+flag=true
 
-echo -e "\e[93m¿Cual es su primer apellido?\e[0m"
-read apellido
+while $flag
+do
+    echo -e "\e[92m¿Cual es su nombre?\e[0m"
+    read nombre
+    echo -e "\e[92m¿Cual es su primer apellido?\e[0m"
+    read apellido
+    
+    if [[ -z "$nombre" || -z "$apellido" ]];
+    then
+        clear
+        echo -e "\e[91mFavor de verificar los datos ingresados...\e[0m"
+    else
+        echo -e "\e[92mHola $nombre $apellido\e[0m"
+        flag=false
+    fi
+    
+done
 
-echo -e "\e[104mHola $nombre $apellido\e[0m"
+#Funcion para obtener el directorio actual donde estamos ubicados
+function get_directorio(){
+    echo -e "\e[91m*** Directorio actual ***\e[0m"
+    progress_bar
+    pwd
+}
+get_directorio
 
-echo -e "\e[91m*** Directorio actual ***\e[0m"
-progress_bar
-pwd
+#Funcion para probar la conectividad a la computadora local 127.0.0.1
+function ping_local(){
+    echo -e "\e[91m*** Probando la conectividad con un ping a la computadora local 127.0.0.1 ***\e[0m"
+    ping -c 4 127.0.0.1 > ping.txt
+    progress_bar
+    echo -e "\e[92mSe genero un archivo en $HOME/ping.txt con el resultado obtenido\e[0m"
+}
+ping_local
 
-echo -e "\e[91m*** Probando la conectividad con un ping a la computadora local 127.0.0.1 ***\e[0m"
-ping -c 4 127.0.0.1 > ping.txt
-progress_bar
-echo -e "\e[92mSe genero un archivo en $HOME/ping.txt con el resultado obtenido\e[0m"
+#Funcion para obtener el tamaño del directorio solicitado
+function get_size(){
+    echo -e "\e[91m*** Consultado el tamaño del directorio home del usuario ***\e[0m"
+    du -sh $HOME > size.txt
+    progress_bar
+    echo -e "\e[92mSe genero un archivo en $HOME/size.txt con el resultado obtenido\e[0m"
+}
+get_size
 
-echo -e "\e[91m*** Consultado el tamaño del directorio home del usuario ***\e[0m"
-du -sh $HOME > size.txt
-progress_bar
-echo -e "\e[92mSe genero un archivo en $HOME/size.txt con el resultado obtenido\e[0m"
+#Funcion para crear algunos archivos .txt
+function new_files(){
+    echo -e "\e[91m*** Creando 3 archivos .txt ***\e[0m"
+    touch lab{1..3}.txt
+    progress_bar
+    echo -e "\e[92mLos archivos lab{1..3}.txt estan en el directorio actual $HOME\e[0m"
+}
+new_file
 
-echo -e "\e[91m*** Creando 3 archivos .txt ***\e[0m"
-touch lab{1..3}.txt
-progress_bar
-echo -e "\e[92mSe generaron lab{1..3}.txt en el directorio actual $HOME\e[0m"
+#Funcion para crear un nuevo directorio solicitando primero el nombre y 
+#verificar que no exista ya en el directorio home
+function new_directorio(){
+    flag=true
+    while $flag
+    do
+        echo -e "\e[91m*** Nuevo directorio ***\e[0m"
+        echo -e "\e[92mIngresar el nombre del directorio\e[0m"
+        read directorio
+        
+        if [[ -z "$directorio" ]];
+        then
+            clear
+            echo -e "\e[91mFavor de verificar los datos ingresados...\e[0m"
+        else
+            if [[ -e $HOME/$directorio ]];
+            then
+                clear
+                echo -e "\e[91mYa existe un elemento con el nombre especificado: $HOME/$directorio\e[0m"
+            else
+                mkdir $directorio
+                progress_bar
+                echo -e "\e[92mSe genero un nuevo directorio en la ubicacion de $HOME/$directorio\e[0m"
+                flag=false
+            fi            
+        fi
 
-echo -e "\e[91m*** Nuevo directorio ***\e[0m"
-echo -e "\e[93mIngresar el nombre del directorio\e[0m"
-read directorio
+    done            
+}
+new_directorio
 
-mkdir $directorio
-progress_bar
-echo -e "\e[92mSe genero un nuevo directorio en la ubicacion de $HOME/$directorio\e[0m"
+#Funcion para copiar los archivos .txt en el nuevo directorio
+function copy_new_files(){
+    echo -e "\e[91m*** Copiando los tres archivos generados anteriomente ***\e[0m"
+    cp lab{1..3}.txt $HOME/$directorio
+    progress_bar
+    echo -e "\e[92mSe copiaron los archivo lab{1..3}.txt en la siguiente ubicacion $HOME/$directorio\e[0m"
+}
+copy_new_files
 
-echo -e "\e[91m*** Copiando los tres archivos generados anteriomente ***\e[0m"
-cp lab{1..3}.txt $HOME/$directorio
-progress_bar
-echo -e "\e[92mSe copiaron los archivo lab{1..3}.txt en la siguiente ubicacion $HOME/$directorio\e[0m"
+#Funcion para copiar los archivos generado en el nuevo directorio creado
+function copy_files(){
+    echo -e "\e[91m*** Copiando los archivos ping.txt y size.txt en el nuevo directorio ***\e[0m"
+    cp ping.txt size.txt $HOME/$directorio
+    progress_bar
+    echo -e "\e[92mSe copiaron los archivos ping.txt y size.txt en $HOME/$directorio\e[0m"
+}
+copy_files
 
-echo -e "\e[91m*** Copiando los archivos ping.txt y size.txt en el nuevo directorio ***\e[0m"
-cp ping.txt size.txt $HOME/$directorio
-progress_bar
-echo -e "\e[92mSe copiaron los archivos ping.txt y size.txt en $HOME/$directorio\e[0m"
+#Funcion para obtener el contenido del nuevo directorio
+function read_directorio(){
+    echo -e "\e[91m*** Listando el contenido del nuevo directorio ***\e[0m"
+    progress_bar
+    tree -a $HOME/$directorio
+}
+read_directorio
 
-echo -e "\e[91m*** Listando el contenido del nuevo directorio ***\e[0m"
-progress_bar
-tree -a $HOME/$directorio
-
+#Llamar al script consumidor.sh y enviar parametros
 echo -e "\e[91m*** Llamando al script consumidor.sh y enviando parametros ***\e[0m"
 progress_bar
 sh ./consumidor.sh $nombre $apellido
 
 exit 0
-
